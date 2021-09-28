30b8a56de9175348ec8f9e430e99a623305bd621
Tarang <tarang.com@gmail.com>
2021-09-28 15:21:57 +1000

    Clean it up a bit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var request = require("request");
+var axios = require("axios");
 var _ = require("lodash");
 var address = "16DaLhUXvrhsbvptGkJtcprbTdrp5sqpFe";
 var block_url = "https://blockchain.info/block/000000000000000013eeb37e234acc365dd883e526af8c660ac740d2e3894826?format=json";
@@ -8,43 +8,33 @@ function ab2str(buf) {
   return String.fromCharCode.apply(null, new Uint16Array(buf));
 }
 
-request({
-	url: block_url,
-	json: true,
-	method: "GET"
-}, function (error, response, block) {
-	if (!error && response.statusCode == 200) {
-		var transactions = _(block.tx).filter(function(transaction) {	
-			return ((_.where(transaction.out, {addr: address}).length > 0)
-			)
-		}).map(function(transaction) {
-			var scripts = _(transaction.out).pluck("script").filter(function(script) {
-				return (script.slice(0,2) == "6a")
-			}).map(function(script) {
-				var bufferData = new Buffer(script.slice(4, 84), "hex");  
-				return ab2str(bufferData).match(/C\+([\d]*)V([\@\|\.\&\*\-\(\)\w\n\/\s\:\,]*)\=/);
-			}).filter(function(set) {
-				return set;
-			}).map(function(doc) {
-				return [
-					parseInt(doc[1]),
-					doc[2]
-				]
-			}).value();
+let run = async () => {
 
-			return scripts[0];
-		}).sortBy(function(doc) {
-			return doc[0];
-		}).pluck(1).value().join('').split("\n");
+  let { data } = await axios.get(block_url);
+  
+  let transactions = data.tx.filter(tx => tx.out.filter(out => out.addr === address).length > 0);
 
-		//Some post
-		var statement = _(transactions).map(function(line, index) {
-			if(index < 2) line = line.split('').reverse().join('').green
-			return line.replace("moc.gnarat", "tarang.com");
-		}).join("\n");
+  console.log(transactions);
 
-		console.log(statement.bold)
+  var scripts = transactions
+    .map(tx => tx.out.map(i => i.script))
+    .filter(script => script.map(item => item.slice(0, 2) == "6a").filter(item => item).length > 0)
+    .map(script => script.map(item => item.slice(4, 84), "hex"))
+    .map(script => script.map(item => ab2str(Buffer.from(item, "hex"))))
+    .map(script => script.map(item => item.match(/C\+([\d]*)V([\@\|\.\&\*\-\(\)\w\n\/\s\:\,]*)\=/)))
+    .map(script => [parseInt(script[0][1]), script[0][2]])
+    .sort((a, b) => a[0] - b[0])
+    .map(script => script[1])
+    .join('')
+    .split('\n')
 
+  var statement = scripts.map((line, index) => {
+    if(index < 2) line = line.split('').reverse().join('').green
+    return line.replace("moc.gnarat", "tarang.com");
+  }).join("\n");
 
-	}
-})
+  console.log(statement.bold)
+
+}
+
+run();
